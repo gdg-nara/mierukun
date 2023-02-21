5ebaa2912070a2273d4326be9f0096f89ff5dac4
tanykazy <tanykazy@gmail.com>
2023-02-21 18:01:33 +0900

    グラフ再描画を軽量化

--- src/app/components/kokuban-chart/kokuban-chart.component.ts
+++ src/app/components/kokuban-chart/kokuban-chart.component.ts
@@ -8,12 +8,41 @@ declare const google: any;
   styleUrls: ['./kokuban-chart.component.css']
 })
 export class KokubanChartComponent implements OnInit {
-
-  private treeDataTable = new Map<string, number>();
+  private readonly chartaria = 'chart-aria';
+  private dataTable!: Map<string, number>;
+  private treeDataTable: any;
+  private treeMap: any;
+  private treeMapOptions = {
+    enableHighlight: true,
+    maxDepth: 1,
+    maxPostDepth: 1,
+    // minHighlightColor: '#8c6bb1',
+    // midHighlightColor: '#9ebcda',
+    // maxHighlightColor: '#edf8fb',
+    hintOpacity: 0.1,
+    maxColor: '#ee8100',
+    minColor: '#009688',
+    midColor: '#f7f7f7',
+    headerHeight: 0,
+    showScale: false,
+    showTooltips: true,
+    // height: 500,
+    useWeightedAverageForAggregation: true,
+    // Use click to highlight and double-click to drill down.
+    eventsConfig: {
+      highlight: ['click'],
+      unhighlight: ['mouseout'],
+      rollup: ['contextmenu'],
+      drilldown: ['dblclick'],
+    },
+    generateTooltip: (row: any, size: any, value: any) => {
+      return '';
+    }
+  };
 
   @Input() set data(data: Map<string, number>) {
     if (data) {
-      this.treeDataTable = data;
+      this.dataTable = data;
     }
   }
 
@@ -30,49 +59,25 @@ export class KokubanChartComponent implements OnInit {
   }
 
   private drawChart(): void {
-    if (this.treeDataTable.size > 0) {
+    if (this.dataTable.size > 0) {
       let color = Math.random();
       const dataArray: Array<any> = [
         ['Kind', 'Parent', 'Time', 'Color'],
         ['root', null, NaN, NaN]
       ];
-      for (const [label, value] of this.treeDataTable) {
+      for (const [label, value] of this.dataTable) {
         dataArray.push([label, 'root', value, color++]);
       }
-      const dataTable = new google.visualization.arrayToDataTable(dataArray);
-      const tree = new google.visualization.TreeMap(document.getElementById('chart-aria'));
+      this.treeDataTable = new google.visualization.arrayToDataTable(dataArray);
+      this.treeMap = new google.visualization.TreeMap(document.getElementById(this.chartaria));
 
-      tree.draw(dataTable, {
-        enableHighlight: true,
-        maxDepth: 1,
-        maxPostDepth: 1,
-        minHighlightColor: '#8c6bb1',
-        midHighlightColor: '#9ebcda',
-        maxHighlightColor: '#edf8fb',
-        minColor: '#009688',
-        midColor: '#f7f7f7',
-        maxColor: '#ee8100',
-        headerHeight: 0,
-        showScale: false,
-        // height: 500,
-        useWeightedAverageForAggregation: true,
-        // Use click to highlight and double-click to drill down.
-        eventsConfig: {
-          highlight: ['click'],
-          unhighlight: ['mouseout'],
-          rollup: ['contextmenu'],
-          drilldown: ['dblclick'],
-        },
-        generateTooltip: (row: any, size: any, value: any) => {
-          return '';
-        }
-      });
+      this.treeMap.draw(this.treeDataTable, this.treeMapOptions);
     }
   }
 
   // window.resize イベントでグラフを再描画する
   @HostListener('window:resize', ['$event'])
-  onWindowResize(event: UIEvent) {
-    this.drawChart();
+  onWindowResize(event: UIEvent): void {
+    this.treeMap.draw(this.treeDataTable, this.treeMapOptions);
   }
 }
