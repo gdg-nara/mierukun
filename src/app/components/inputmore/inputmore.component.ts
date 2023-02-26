195099fd4029e932a93e037aaf647502abc95cbb
tanykazy <tanykazy@gmail.com>
2023-02-26 22:15:22 +0900

    inputmoreコンポーネントにコメントを追加

--- src/app/components/inputmore/inputmore.component.ts
+++ src/app/components/inputmore/inputmore.component.ts
@@ -9,15 +9,30 @@ import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
   styleUrls: ['./inputmore.component.css']
 })
 export class InputmoreComponent {
+  // Input に表示するラベル
   @Input() label!: string;
+
+  // Input に表示するプレイスホルダ
   @Input() placeholder!: string;
 
+  // 入力済みの名前
   @Input() names!: Array<string>;
+
+  // 入力値が変化したときに発行するイベント
+  // 双方向バインディングに必要
   @Output() namesChange = new EventEmitter<string[]>();
 
+  // Input からフォーカスが外れていてもイベント発行する
   readonly addOnBlur: boolean = true;
+
+  // 入力値を区切るセパレータ
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
 
+  /**
+   * 入力を処理してチップを追加する
+   * すでに存在する値は登録しない
+   * @param {MatChipInputEvent} event - イベントオブジェクト
+   */
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
     if (value) {
@@ -31,6 +46,10 @@ export class InputmoreComponent {
     event.chipInput!.clear();
   }
 
+  /**
+   * チップを削除する 
+   * @param {string} name - チップの名前
+   */
   remove(name: string): void {
     const index = this.names.indexOf(name);
     if (index !== -1) {
@@ -39,6 +58,12 @@ export class InputmoreComponent {
     }
   }
 
+  /**
+   * チップの値が編集されたときに呼び出されるイベントハンドラ
+   * 編集後の値が重複している場合、編集結果を破棄する
+   * @param name - チップの名前
+   * @param {MatChipEditedEvent} event - イベントオブジェクト
+   */
   edit(name: string, event: MatChipEditedEvent): void {
     const value = event.value.trim();
     if (!value) {
@@ -56,6 +81,11 @@ export class InputmoreComponent {
     }
   }
 
+  /**
+   * チップをドラッグアンドドロップしたときに呼び出されるイベントハンドラ
+   * ドラッグアンドドロップ後の状態に並び替える
+   * @param {CdkDragDrop<string[]>} event - イベントハンドラ
+   */
   drop(event: CdkDragDrop<string[]>): void {
     moveItemInArray(this.names, event.previousIndex, event.currentIndex);
     this.namesChange.emit(Array.from(this.names));
