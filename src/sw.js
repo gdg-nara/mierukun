7b36d8eeffd339a312e332c57c5eb7e51ab38981
t-cool <t-cool@t-coolnoMacBook-Pro.local>
2023-03-10 17:31:02 +0900

    merge

---
+++ src/sw.js
@@ -0,0 +1,16 @@
+//'use strict';
+
+/* global registration */
+
+self.addEventListener('install', function (event) {
+    // skip
+  });
+  
+  self.addEventListener('activate', function (event) {
+    // skip
+  });
+  
+  self.addEventListener('fetch', function (event) {
+    // skip
+  });
+  
