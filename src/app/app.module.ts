cba0dd1532777948839baa64b6e62884a63d32ce
tanykazy <tanykazy@gmail.com>
2023-03-12 20:49:48 +0900

    importを整理

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule, isDevMode } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
@@ -10,26 +13,19 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // 学習活動を記録するボタンのコンポーネント
 import { ButtonsetComponent } from './components/buttonset/buttonset.component';
-
-// ボタン
-import { MatButtonModule } from '@angular/material/button';
-// テキストインプット
-import { MatInputModule } from '@angular/material/input';
-// Forms
-import { FormsModule } from '@angular/forms';
 // ユーザの入力に応じて追加入力が可能なテキストインプットコンポーネント
 import { InputmoreComponent } from './components/inputmore/inputmore.component';
 // 黒板面積グラフを表示するためのコンポーネント
 import { KokubanChartComponent } from './components/kokuban-chart/kokuban-chart.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
